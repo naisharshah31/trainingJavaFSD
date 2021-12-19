5ed895031cf83c1a6e45a78edae17aa253352378
Naishar Shah <Naishar.Shah@trivium-esolutions.com>
2021-12-19 16:53:47 +0530

    Create User class with validations

---
+++ CompanyLockersAssignment/src/com/lockers/LockerMain.java
@@ -0,0 +1,13 @@
+package com.lockers;
+
+public class LockerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Users user = new Users("naishar", "shah", "naishar.shah31@gmail.com", "9341111241");
+
+	}
+
+}
