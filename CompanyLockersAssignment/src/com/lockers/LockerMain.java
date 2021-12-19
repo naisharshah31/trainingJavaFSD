ebd88cc2e5641cec8d6a3f752390ef2be306d6c5
Naishar Shah <Naishar.Shah@trivium-esolutions.com>
2021-12-20 02:00:54 +0530

    Working prototype with modularization and meaningful comments

--- CompanyLockersAssignment/src/com/lockers/LockerMain.java
+++ CompanyLockersAssignment/src/com/lockers/LockerMain.java
@@ -1,27 +1,9 @@
 package com.lockers;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class LockerMain {
-
-	//Insertion sort
-	private static String[] sort_sub(String array[], int f){
-		String temp="";
-		for(int i=0;i<f;i++){
-			for(int j=i+1;j<f;j++){
-				if(array[i].compareToIgnoreCase(array[j])>0){
-					temp = array[i];
-					array[i]=array[j];
-					array[j]=temp;
-				}
-			}
-		}
-		return array;
-	}
 	
 	public static void main(String[] args) {
 		
@@ -29,20 +11,21 @@ public class LockerMain {
 		System.out.println("Developer Name: Naishar Shah");
 		System.out.println("Designation: Intern at Trivium eSolutions");
 		
+		//input for service request number
 		int choice;
 		
 		//Object creation
 		Scanner scan = new Scanner(System.in);
 		
-		
 		while(true) {
 			
+			//Options
 			System.out.println("\n ********************** Company Lockers Pvt. Ltd. ******************************");
 			System.out.println("1. List files (Asc.) ");
 			System.out.println("2. Delete a file");
 			System.out.println("3. Search a file");
 			System.out.println("4. Create a file");
-			System.out.println("4. Exit ");
+			System.out.println("5. Exit ");
 			
 			System.out.print("Please enter your choice : ");
 			
@@ -52,81 +35,43 @@ public class LockerMain {
 			switch(choice) {
 				
 				case 1:
-					int fileCount = 0;
-				    ArrayList<String> filenames = new ArrayList<String>();
 					
-
-
-					File directoryPath = new File(System.getProperty("user.dir"));
-					File[] listOfFiles = directoryPath.listFiles();
-					fileCount = listOfFiles.length;
-					
-					
-					System.out.println("All files: ");
-					for (int i = 0; i < fileCount; i++) {
-					  if (listOfFiles[i].isFile()) {
-					    filenames.add(listOfFiles[i].getName());
-					  } 
-					}
-					
-					String[] str = new String[filenames.size()];
-					 
-			        for (int i = 0; i < filenames.size(); i++) {
-			            str[i] = filenames.get(i);
-			        }
-					String[] sorted_filenames = sort_sub(str, str.length);
-					
-					for(String currentFile: sorted_filenames) {
-						System.out.println(currentFile);
-					}
-					
+					//List function feature to list all files in ascending order.
+					FileFunctions.listFiles();
 					break;
 				
 				case 2:
 					
+					//Input for filename to be deleted
 					System.out.print("Enter filename to be deleted: ");
 					String fileToBeDeleted = scan.next();
 					
-					File file = new File( (System.getProperty("user.dir") ) + "\\" + fileToBeDeleted );
-					if(file.exists()) {
-						if ( file.delete() ) {
-							System.out.println("File deleted successfully!");
-						}
-					} else {
-						System.out.println("Error 404 (Not Found)");
-					}
+					//Function to delete a specific file given by user
+					FileFunctions.deleteFile(fileToBeDeleted);
 					break;
 				
 				case 3:
 					
+					//Input for filename to be searched
 					System.out.println("Please enter filename to be searched: ");
 					String fileToBeSearched = scan.next();
-					file = new File( (System.getProperty("user.dir") ) + "\\" + fileToBeSearched );
-					if(file.exists()) {
-						System.out.println("File exits!");
-					} else {
-						System.out.println("Error 404 (Not Found)");
-					}
+					
+					//Function to search a specific file given by user
+					FileFunctions.searchFile(fileToBeSearched);
 					break;
 				
 				case 4:
 					
+					//Input for filename to be created
 					System.out.println("Please enter filename to be created: ");
 					String fileToBeCreated = scan.next();
-					file = new File( (System.getProperty("user.dir") ) + "\\" + fileToBeCreated );
 					
-				try {
-					if (file.createNewFile() ) {
-						System.out.println("File Created!");
-					} else {
-						System.out.println("File already exists :(");
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+					//Function to create a specific file given by user
+					FileFunctions.createFile(fileToBeCreated);
+					break;
 					
 				case 5:
+					
 					//Releasing scanner resource
 					scan.close();
 					System.out.println("\n ****************** Exiting ************************");
@@ -135,9 +80,7 @@ public class LockerMain {
 				
 				default:
 					//Releasing scanner resource
-					scan.close();
-					System.out.println("\n ****************** Exiting ************************");
-					System.exit(1);
+					System.out.println("\n ****************** Please enter an option between 1-5 ************************");
 					break;
 				
 					
